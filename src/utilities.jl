19b3303945b161f04c275b7c7641952d66002186
Tamas K. Papp <tkpapp@gmail.com>
2018-04-06 09:05:49 +0200

    Removed local_test.
    Obsoleted by RoguePkg.jl.

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,6 +1,5 @@
 export
     showfull,
-    local_test,
     edit_list
 
 """
@@ -11,36 +10,6 @@ Print arguments without truncation.
 """
 showfull(io, x) = show(IOContext(io; compact = false, limit = false), x)
 showfull(x) = showfull(STDOUT, x)
-
-"""
-    local_test(pkgname, [coverage])
-
-Find and test a package in `LOAD_PATH`. Useful when the package is outside
-`Pkg.dir()`.
-
-Assumes the usual directory structure: package has the same name as the module,
-the main file is in `src/Pkgname.jl`, while tests are in `test/runtests.jl`.
-"""
-function local_test(pkgname; coverage::Bool=false)
-    module_path = Base.find_in_path(pkgname, nothing)
-    @assert module_path != nothing "Could not find \"$(pkgname)\" in path"
-    src_dir, module_file = splitdir(module_path)
-    dir = normpath(src_dir, "..")
-    test_path = joinpath(dir, "test", "runtests.jl")
-    @assert isfile(test_path) "Could not find $(test_path)"
-    Base.cd(dir) do
-        try
-            color = Base.have_color? "--color=yes" : "--color=no"
-            codecov = coverage? ["--code-coverage=user"] : ["--code-coverage=none"]
-            compilecache = "--compilecache=" * (Bool(Base.JLOptions().use_compilecache) ? "yes" : "no")
-            julia_exe = Base.julia_cmd()
-            run(`$julia_exe --check-bounds=yes $codecov $color $compilecache $test_path`)
-            info("$module_file tests passed")
-        catch err
-            Base.Pkg.Entry.warnbanner(err, label="[ ERROR: $module_file ]")
-        end
-    end
-end
 
 function _edit_list(ms, n)
     if isempty(ms)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,2 @@
 using TKPappKitchenSink
 using Base.Test
-
-# please don't name your package like this :P
-@test_throws AssertionError local_test("NonExistentPackage_66a0b5a72a90c23b")
